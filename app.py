7eb11aae94c1ff346ac1a79d65a46eb2b9824198
TonyIsHuman <126887427+TonyIsHuman@users.noreply.github.com>
2023-03-17 21:59:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,10 @@ import pickle
 import numpy as np
 model=pickle.load(open('model.pickle','rb'))
 st.title("Revenue Prediction")
-x=st.number_input('Input Temperature')
+X_new=st.number_input('Input Temperature')
 if st.button('Predict'):
-  x=np.array(x).reshape(-1,1)
-  result=model.predict(x)
+  X_new=np.array(X_new).reshape(-1,1)
+  y_new=model.predict(X_new)
   st.caption('Revenue Prediction')
-  ket_qua=result[0]
-  ket_qua=str(ket_qua)
-  st.success(ket_qua)
+  st.write(y_new)
+  
